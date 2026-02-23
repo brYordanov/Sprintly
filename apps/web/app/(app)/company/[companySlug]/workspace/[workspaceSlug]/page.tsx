6771b1ddf9a9d297098ccb3a2b93f8d93c9665a8
BranimirYo <branimiryordanov75@gmail.com>
2026-02-23 22:03:35 +0200

    workspace fe detail page

---
+++ apps/web/app/(app)/company/[companySlug]/workspace/[workspaceSlug]/page.tsx
@@ -0,0 +1,8 @@
+export default async function WorkspaceDetailsPage({
+    params,
+}: {
+    params: Promise<{ workspaceSlug: string }>
+}) {
+    const { workspaceSlug } = await params
+    return <div className="p-8 space-y-6">{workspaceSlug}</div>
+}
